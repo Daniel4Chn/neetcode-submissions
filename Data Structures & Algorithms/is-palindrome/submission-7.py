628ea793f7dc65735137dacc52ee1fdb4fb6bfd5
Daniel Chan <93110815+Daniel4Chn@users.noreply.github.com>
2026-09-23 16:50:50 -0400

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+
+        s = s.lower()
+        i = 0
+        j = len(s)-1
+        while i < j:
+            if self.isCharOrNot(s[i]) == False:
+                i+=1
+                continue
+            if self.isCharOrNot(s[j]) == False:
+                j-=1
+                continue
+            if s[i] != s[j]:
+                return False
+            else:
+                i+=1
+                j-=1
+        return True
+        
+
+    def isCharOrNot(self, s) -> bool:
+        if ord('Z') >= ord(s) and ord(s) >= ord('A'):
+            return True
+        elif ord('z') >= ord(s) and ord(s) >= ord('a'):
+            return True
+        elif ord('9') >= ord(s) and ord(s) >= ord('0'):
+            return True
+        else:
+            return False
